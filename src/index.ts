9adf563eed8cf0f36c3d517650f79d3e27047a4d
BengBu-YueZhang <zhangyue_coder@163.com>
2020-02-02 17:35:03 +0800

    添加新的命令

--- src/index.ts
+++ src/index.ts
@@ -11,28 +11,28 @@ export function activate(context: vscode.ExtensionContext) {
 	// "onCommand:extension.TSandReactandClass",
 	// "onCommand:extension.TSandVue3"
 
-	const JSandVue2 = vscode.commands.registerCommand('extension.JSandVue2', () => {
+	const JSandVue2 = vscode.commands.registerCommand('quickstart.JSandVue2', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
-	const JSandVue2andClass = vscode.commands.registerCommand('extension.JSandVue2andClass', () => {
+	const JSandVue2andClass = vscode.commands.registerCommand('quickstart.JSandVue2andClass', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
-	const TSandVue2 = vscode.commands.registerCommand('extension.TSandVue2', () => {
+	const TSandVue2 = vscode.commands.registerCommand('quickstart.TSandVue2', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
-	const JSandReact = vscode.commands.registerCommand('extension.JSandReact', () => {
+	const JSandReact = vscode.commands.registerCommand('quickstart.JSandReact', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
-	const JSandReactandClass = vscode.commands.registerCommand('extension.JSandReactandClass', () => {
+	const JSandReactandClass = vscode.commands.registerCommand('quickstart.JSandReactandClass', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
-	const TSandReact = vscode.commands.registerCommand('extension.TSandReact', () => {
+	const TSandReact = vscode.commands.registerCommand('quickstart.TSandReact', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
-	const TSandReactandClass = vscode.commands.registerCommand('extension.TSandReactandClass', () => {
+	const TSandReactandClass = vscode.commands.registerCommand('quickstart.TSandReactandClass', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
-	const TSandVue3 = vscode.commands.registerCommand('extension.TSandVue3', () => {
+	const TSandVue3 = vscode.commands.registerCommand('quickstart.TSandVue3', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
 
